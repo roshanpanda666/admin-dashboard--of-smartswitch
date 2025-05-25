8fdae146ffc49e6a568867de02951730fdf5b7aa
roshan <roshanpanda666@gmail.com>
2025-05-25 21:24:16 +0530

    time stamp update

--- app/components/Timelog.js
+++ app/components/Timelog.js
@@ -30,9 +30,9 @@ export default function Timelog() {
       <h1 className="text-2xl font-bold mb-4">All Status Data</h1>
       <div className="space-y-4 ">
       <div className="justify-center items-center text-center flex lg:gap-[10vw] gap-[20vw] bg-[#0F1727] py-5 rounded-3xl">
-                    <div className="w-full"> Time stamp</div>
-                    <div className="w-full">medium</div>
-                    <div className="w-full">status</div>
+                    <div className="lg:w-full w-[10%] text-center">Time stamp</div>
+                    <div className="lg:w-full w-[10%] text-center">Medium</div>
+                    <div className="lg:w-full w-[10%] text-center mr-4">Status</div>
                 </div>
                 
         {data.map((item, index) => (
@@ -41,7 +41,7 @@ export default function Timelog() {
             className="border-b-[1px] border-[#273464] p-4 rounded shadow "
           >
             <div className="flex justify-center items-center lg:gap-[10vw]">
-                <div className="text-white w-full text-center">{item.timestamp}</div>
+                <div className="text-white w-full text-center"> {new Date(item.timestamp).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' })}</div>
                 <div className="text-white w-full text-center">{item.relay}</div>
                 <div className="text-white w-full text-center">{item.medium}</div>
                 
